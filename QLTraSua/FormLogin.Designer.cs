c84c55af9494f7c0abaf627c9e271f31ef115699
QuocHuy <qhuynehihi@gmail.com>
2023-06-06 11:23:43 +0700

    Cập nhật FormLogin lần thứ n

--- QLTraSua/FormLogin.Designer.cs
+++ QLTraSua/FormLogin.Designer.cs
@@ -220,6 +220,7 @@
             this.Name = "FormLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormLogin";
+            this.Load += new System.EventHandler(this.FormLogin_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
--- QLTraSua/FormLogin.cs
+++ QLTraSua/FormLogin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,10 @@ namespace QLTraSua
 {
     public partial class FormLogin : Form
     {
+        SqlConnection conn;
+        string strConnectionString = "Data Source=(local)\\SQLEXPRESS;Initial Catalog=QLQUANTRASUA;"
+            + "Integrated Security=True";
+        SqlCommand com;
         public FormLogin()
         {
             InitializeComponent();
@@ -19,7 +24,46 @@ namespace QLTraSua
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
+            try
+            {
+                string cmd = "Select TaiKhoan, MatKhau From DangNhap ";
+                cmd += "where TaiKhoan='" + txtUser.Text.Trim() + "'";
+                cmd += " and MatKhau='" + txtPass.Text.Trim() + "'";
+                com.CommandText = cmd;
+                SqlDataReader check = com.ExecuteReader();
 
+                if (check.HasRows)
+                {
+                    MessageBox.Show("Đăng nhập thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Sai Tài Khoản hoặc Mật Khẩu", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtUser.Focus();
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(strConnectionString);
+                if (conn.State == ConnectionState.Open) conn.Close();
+                conn.Open();
+                com = new SqlCommand();
+                com.Connection = conn;
+                txtPass.PasswordChar = '*';
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
     }
 }
